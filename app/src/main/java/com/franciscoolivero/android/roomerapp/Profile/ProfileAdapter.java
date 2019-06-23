f4e5f007e7cd25882d84747205a49f7e72719743
Francisco Olivero <franj.olivero@gmail.com>
2019-06-22 21:43:10 -0300

    [Resultados] [ProfileAdapter] Codigo base para el adapter de las cartas de usuario

--- app/src/main/java/com/franciscoolivero/android/roomerapp/Profile/ProfileAdapter.java
+++ app/src/main/java/com/franciscoolivero/android/roomerapp/Profile/ProfileAdapter.java
@@ -5,13 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.franciscoolivero.android.roomerapp.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProfileAdapter extends ArrayAdapter<Profile> {
 
@@ -24,29 +31,27 @@ public class ProfileAdapter extends ArrayAdapter<Profile> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listItemView = convertView;
         if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.result_list_item, parent, false);
         }
         final Profile currentProfile = getItem(position);
-//        ViewHolder holder = new ViewHolder(listItemView);
-//
-//        holder.tittle.setText(currentProfile.getmTitle());
+        ViewHolder holder = new ViewHolder(listItemView);
 
         //TODO LOGIC FOR PROFILE CARD.
-//        if (currentProfile.hasAuthor()) {
-//            holder.author.setVisibility(View.VISIBLE);
-//
-//            String authorString = TextUtils.join(", ", currentProfile.getmAuthors());
-//            holder.author.setText(authorString);
-//        } else {
-//            holder.author.setVisibility(View.GONE);
-//        }
-//
-//        if (currentProfile.hasImage()) {
-//            Picasso.with(getContext()).load(currentProfile.getmImage()).into(holder.profileImage);
-//        } else {
-//            //TODO
-//        }
-//
+
+        //Get and update the name and full name
+        String userfullName = currentProfile.getmName() + " " + currentProfile.getmLastName();
+        holder.nameTextView.setText(userfullName);
+
+        //Get and update the user age
+        holder.ageTextView.setText(String.valueOf(currentProfile.getmAge()));
+
+        //If there is an image, get it using picasso and set it to the ImageView
+        if (currentProfile.hasImage()) {
+            Picasso.get().load(currentProfile.getmPicture()).into(holder.profileImageView);
+        } else {
+            //TODO
+        }
+
 //        if (currentProfile.isSaleable()) {
 //            holder.amount.setVisibility(View.VISIBLE);
 //            holder.currencyCode.setVisibility(View.VISIBLE);
@@ -74,32 +79,55 @@ public class ProfileAdapter extends ArrayAdapter<Profile> {
 //                openWebPage(currentProfile);
 //            }
 //        });
+
+
+
+        //TODO - Add more Complex Logic for button pressing and disabling each card.
+//        buttonSale.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                int quantity = Integer.valueOf(holder.quantity.getText().toString());
+//                long affectedRowOrId;
+//
+//                if (quantity != 0) {
+//                    quantity -= 1;
+//                }
+//
+//                String strQuantity = String.valueOf(quantity);
+//
+//                // Create a new map of values, where column names are the keys
+//                ContentValues values = new ContentValues();
+//
+//                values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, strQuantity);
+//
+//                affectedRowOrId = context.getContentResolver().update(mCurrentProductUri, values, null, null);
+//                notifyDataSetChanged();
+//
+//                if (affectedRowOrId == 0) {
+//                    Toast toast = Toast.makeText(view.getContext(), "Error selling product", Toast.LENGTH_SHORT);
+//                    toast.show();
+//                }
+//            }
+//        });
         return listItemView;
     }
 
-//    static class ViewHolder {
-//        @BindView(R.id.text_tittle)
-//        TextView tittle;
-//        @BindView(R.id.text_author)
-//        TextView author;
-//        @BindView(R.id.text_rating)
-//        TextView rating;
-//        @BindView(R.id.text_price_amount)
-//        TextView amount;
-//        @BindView(R.id.text_currency)
-//        TextView currencyCode;
-//        @BindView(R.id.image_profile)
-//        ImageView profileImage;
-//        @BindView(R.id.image_star)
-//        ImageView starRatingImage;
+    static class ViewHolder {
+        @BindView(R.id.image_card_view)
+        CardView imageCardViewContainer;
+        @BindView(R.id.image_list_item)
+        ImageView profileImageView;
+        @BindView(R.id.name_list_item)
+        TextView nameTextView;
+        @BindView(R.id.age_list_item)
+        TextView ageTextView;
+        @BindView(R.id.button_add_list_item)
+        ImageButton addLikeButton;
 
-//        @BindView(R.id.button_buy)
-//        Button buttonBuy;
-
-//        private ViewHolder(View view) {
-//            ButterKnife.bind(this, view);
-//        }
-//    }
+        private ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
 
 //    private void openWebPage(Profile profile) {
 //        //Uri profileUri = Uri.parse(profile.getmInfoLink()); Replace
